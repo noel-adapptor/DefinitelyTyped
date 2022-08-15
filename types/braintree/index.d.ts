2238972cdd7e98756d0ce9276ceabb49d7d48d68
noel-adapptor <52151981+noel-adapptor@users.noreply.github.com>
2022-08-15 13:58:16 +0800

    Update index.d.ts

--- types/braintree/index.d.ts
+++ types/braintree/index.d.ts
@@ -1560,6 +1560,7 @@ declare namespace braintree {
         facilitatorDetails?: FacilitatorDetails | undefined;
         gatewayRejectionReason?: GatewayRejectionReason | undefined;
         id: string;
+        graphQLId: string;
         lineItems?: TransactionLineItem[] | undefined;
         masterpassCardDetails?:
             | {
